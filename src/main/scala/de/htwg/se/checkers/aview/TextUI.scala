374d06c5242cd30731682c8a394713d30623ec74
WiebkeRaach <wiebke.raach@web.de>
2020-05-29 15:41:32 +0200

    added print of possible commands/help in TextUI

--- src/main/scala/de/htwg/se/checkers/aview/TextUI.scala
+++ src/main/scala/de/htwg/se/checkers/aview/TextUI.scala
@@ -17,9 +17,12 @@ class TextUI(controller: Controller) extends Observer {
       case "move" :: xPosOld :: yPosOld :: xPosNew :: yPosNew :: Nil =>
         controller.move(xPosOld.toInt, yPosOld.toInt, xPosNew.toInt, yPosNew.toInt)
 
-      //case "exit" :: Nil =>
+      case "exit" :: Nil =>
 
-      case _ =>
+      case _ => println("\nPossible Commands:\nnew Round:\t\t\t\t\tStarts a new Round of the game. " +
+        "The current scores will be lost.\nmove old<X,Y> new<X,Y>:\t\tMoves the Piece from the old position to the " +
+        "new position specified\nexit:\t\t\t\t\t\tExit the Game.\n")
+
     }
   }
   override def update(): Unit = println(controller.gameToString)
