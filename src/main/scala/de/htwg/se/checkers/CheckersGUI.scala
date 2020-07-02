4a2c0cf6436632b587fc39e5882621c1e9bd4d7c
Robert <robert.breuer@htwg-konstanz.de>
2020-07-03 00:16:34 +0200

    Added separate main for TUI

--- src/main/scala/de/htwg/se/checkers/CheckersGUI.scala
+++ src/main/scala/de/htwg/se/checkers/CheckersGUI.scala
@@ -3,26 +3,23 @@ package de.htwg.se.checkers
 import com.google.inject.Guice
 import de.htwg.se.checkers.aview.{GUI, TextUI}
 import de.htwg.se.checkers.control.controllerComponent.ControllerTrait
-import de.htwg.se.checkers.control.controllerComponent.controllerBaseImpl.Controller
-import de.htwg.se.checkers.model.GameComponent.GameBaseImpl.{Color, Game}
-import de.htwg.se.checkers.model.GameComponent.GameTrait
+import de.htwg.se.checkers.model.GameComponent.GameBaseImpl.Color
 
-object CheckersTextUI {
+object CheckersGUI {
 
   val injector = Guice.createInjector(new CheckersModule)
-  //val controller = injector.getInstance(classOf[ControllerTrait])
-  //val game = injector.getInstance(classOf[GameTrait])
   val controller = injector.getInstance(classOf[ControllerTrait])
 
   def main(args: Array[String]): Unit = {
 
     println("Started Checkers in TUI Mode")
 
-    //val controller = new Controller(game)
     val tui = new TextUI(controller)
     val gui = new GUI(controller)
     gui.main(Array())
+
     controller.notifyObservers()
+
     var input: String = ""
     if (args.length > 0) input = args(0)
     if(!input.isEmpty) {
---
+++ src/main/scala/de/htwg/se/checkers/CheckersTUI.scala
@@ -0,0 +1,35 @@
+package de.htwg.se.checkers
+
+import com.google.inject.Guice
+import de.htwg.se.checkers.aview.TextUI
+import de.htwg.se.checkers.control.controllerComponent.ControllerTrait
+import de.htwg.se.checkers.model.GameComponent.GameBaseImpl.Color
+
+object CheckersTUI {
+
+  val injector = Guice.createInjector(new CheckersModule)
+  val controller = injector.getInstance(classOf[ControllerTrait])
+
+  def main(args: Array[String]): Unit = {
+
+    println("Started Checkers in TUI Mode")
+
+    val tui = new TextUI(controller)
+    controller.notifyObservers()
+
+    var input: String = ""
+    if (args.length > 0) input = args(0)
+    if(!input.isEmpty) {
+      if (controller.getWinnerColor().isDefined) println("Winner: " + controller.getWinnerColor)
+      else if (controller.getLastMoveColor() == Color.white) print("Next Player: Black\nNext move: ")
+      else print("Next Player: White\nNext move: ")
+      tui.tuiProcessor(input)
+    } else do {
+      if (controller.getWinnerColor().isDefined) println("Winner: " + controller.getWinnerColor())
+      else if (controller.getLastMoveColor() == Color.white) print("Next Player: Black\nNext move: ")
+      else print("Next Player: White\nNext move: ")
+      input = scala.io.StdIn.readLine()
+      tui.tuiProcessor(input)
+    } while (input != "exit")
+  }
+}
