cf2d8b4689a356a8482d7e6e090f7c22ae802555
Robert <robert.breuer@htwg-konstanz.de>
2020-07-02 17:12:55 +0200

    Added dependency Injection in TextUI

--- src/main/scala/de/htwg/se/checkers/CheckersTextUI.scala
+++ src/main/scala/de/htwg/se/checkers/CheckersTextUI.scala
@@ -1,30 +1,35 @@
 package de.htwg.se.checkers
 
-import de.htwg.se.checkers.aview.{GUI, TextUI}
+import com.google.inject.Guice
+import de.htwg.se.checkers.aview.TextUI
 import de.htwg.se.checkers.control.controllerComponent.controllerBaseImpl.Controller
 import de.htwg.se.checkers.model.GameComponent.GameBaseImpl.{Color, Game}
-import de.htwg.se.checkers.model._
+import de.htwg.se.checkers.model.GameComponent.GameTrait
 
 object CheckersTextUI {
+
+  val injector = Guice.createInjector(new CheckersModule)
+  //val controller = injector.getInstance(classOf[ControllerTrait])
+  val game = injector.getInstance(classOf[GameTrait])
 
   def main(args: Array[String]): Unit = {
 
     println("Started Checkers in TUI Mode")
 
-    val controller = new Controller(new Game())
+    val controller = new Controller(game)
     val tui = new TextUI(controller)
-    val gui = new GUI(controller)
-    gui.main(Array())
+    //val gui = new GUI(controller)
+    //gui.main(Array())
     controller.notifyObservers()
     var input: String = ""
     if (args.length > 0) input = args(0)
     if(!input.isEmpty) {
-      if (controller.getWinnerColor().isDefined) println("Winner: " + controller.getWinnerColor().get)
+      if (controller.getWinnerColor().isDefined) println("Winner: " + controller.getWinnerColor)
       else if (controller.getLastMoveColor() == Color.white) print("Next Player: Black\nNext move: ")
       else print("Next Player: White\nNext move: ")
       tui.tuiProcessor(input)
     } else do {
-        if (controller.getWinnerColor().isDefined) println("Winner: " + controller.getWinnerColor().get)
+        if (controller.getWinnerColor().isDefined) println("Winner: " + controller.getWinnerColor())
         else if (controller.getLastMoveColor() == Color.white) print("Next Player: Black\nNext move: ")
         else print("Next Player: White\nNext move: ")
         input = scala.io.StdIn.readLine()
